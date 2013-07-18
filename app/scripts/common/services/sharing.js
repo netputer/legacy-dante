3d204edd69d7fe926a59701a6f41b3b03b2f9676
wangxiao <1988wangxiao@gmail.com>
2013-07-18 15:57:30 +0800

    增加分享到微博，修复部分bug

--- app/scripts/common/services/sharing.js
+++ app/scripts/common/services/sharing.js
@@ -3,7 +3,8 @@ define([], function() {
 return ['$window', function($window) {
     return {
         weibo: function(photo) {
-            $window.open('http://service.weibo.com/share/share.php?appkey=1483181040&relateUid=1727978503&title='+encodeURIComponent(photo.display_name)+'&url=&pic='+encodeURIComponent(photo.path));
+            var content = "来自 @豌豆荚 云相册照片即时上传";
+            $window.open('http://service.weibo.com/share/share.php?appkey=1483181040&relateUid=1727978503&title='+content+'&url=&pic='+encodeURIComponent(photo.path),'newwindow','height=500, width=400');
         }
     };
 }];
