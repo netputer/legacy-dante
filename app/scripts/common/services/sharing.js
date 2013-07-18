fac9e49ad57b0469c83a32575c055e181d64457e
wangxiao <1988wangxiao@gmail.com>
2013-07-18 16:46:21 +0800

    修改部分文案

--- app/scripts/common/services/sharing.js
+++ app/scripts/common/services/sharing.js
@@ -3,8 +3,8 @@ define([], function() {
 return ['$window', function($window) {
     return {
         weibo: function(photo) {
-            var content = "来自 @豌豆荚 云相册照片即时上传";
-            $window.open('http://service.weibo.com/share/share.php?appkey=1483181040&relateUid=1727978503&title='+content+'&url=&pic='+encodeURIComponent(photo.path),'newwindow','height=500, width=400');
+            var content = '来自 @豌豆荚 云相册照片即时上传';
+            $window.open('http://service.weibo.com/share/share.php?appkey=1483181040&relateUid=1727978503&title='+ encodeURIComponent(content) +'&url=&pic='+encodeURIComponent(photo.path),'newwindow','height=500, width=400');
         }
     };
 }];
