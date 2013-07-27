6f75255acaff460b77d466f79b05950d5b007a88
尼奥 <1988wangxiao@gmail.com>
2013-07-27 16:48:21 +0800

    调整jshint

--- app/scripts/messages/filters/group.js
+++ app/scripts/messages/filters/group.js
@@ -10,7 +10,6 @@ return function(input) {
     var output = _(input).groupBy(function(item) {
         return Math.floor(item.date / (3600 * 24)) * 3600 * 24;
     });
-    console.log('group');
     return output;
 };
 
