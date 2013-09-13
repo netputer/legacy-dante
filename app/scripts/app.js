7065aaae470ab6a1ad5f0c04bdd7bdddc7941abc
xiaomengli <lixiaomeng@wandoujia.com>
2013-09-13 21:50:57 +0800

    delete junk code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -44,8 +44,7 @@ READ_ONLY_FLAG = false;
 //>>includeStart("debug", pragmas.debug);
 READ_ONLY_FLAG = !!window.localStorage.getItem('WD_READ_ONLY_FLAG') || READ_ONLY_FLAG;
 //>>includeEnd("debug");
-//'ngSanitize',
-angular.module('wdApp', ['ng', 'ngSanitize',  'wdCommon', 'wd.ui', 'wdAuth', 'wdPhotos', 'wdLanguage', 'wdMessages', 'wdContacts','wdApplications'])
+angular.module('wdApp', ['ng', 'ngSanitize', 'wdCommon', 'wd.ui', 'wdAuth', 'wdPhotos', 'wdLanguage', 'wdMessages', 'wdContacts','wdApplications'])
     .config([   '$routeProvider', '$httpProvider',
         function($routeProvider,   $httpProvider) {
 
