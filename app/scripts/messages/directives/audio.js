6a2d85b7e31e0b73b64fd99c445db947a017cf49
xiaomengli <lixiaomeng@wandoujia.com>
2013-11-14 11:24:37 +0800

    polish mms

--- app/scripts/messages/directives/audio.js
+++ app/scripts/messages/directives/audio.js
@@ -6,12 +6,7 @@ define([
     ], function(template, $, moment, _) {
 'use strict';
 return ['$window', function($window) {
-    var pauseFuncArray = [];
-    var pauseAllAudios = function() {
-        _.each(pauseFuncArray, function(item) {
-            item();
-        });
-    };
+    var audioList = [];
 
     return {
         template: template,
@@ -21,38 +16,48 @@ return ['$window', function($window) {
         },
         link: function($scope, $element, $attributes) {
             var audioElement = $element.find('audio')[0];
+
+            var audio = {
+                formatDuration: function(duration) {
+                    return moment({s: duration}).format('mm:ss');
+                },
+
+                setInitDuration: function() {
+                    $scope.audio.formatedDuration = audio.formatDuration(audioElement.duration);
+                },
+
+                downloadAudio: function() {
+                    $window.location = $scope.audio.content;
+                },
+
+                pause: function() {
+                    audioElement.pause();
+                    $scope.audio.playing = false;
+                    audio.setInitDuration();
+                },
+
+                play: function() {
+                    audioElement.src = $scope.audio.content;
+                    audioElement.play();
+                    $scope.audio.playing = true;
+                }
+            };
             
-            var formatDuration = function(duration) {
-                return moment({s: duration}).format('mm:ss');
-            };
-
-            var setInitDuration = function() {
-                $scope.audio.formatedDuration = formatDuration(audioElement.duration);
-            };
-
-            var downloadAudio = function() {
-                $window.location = $scope.audio.content;
-            };
-
-            var pause = function() {
-                audioElement.pause();
-                $scope.audio.playing = false;
-                setInitDuration();
-            };
+            
 
             $scope.audio.playing = false;
 
             if (audioElement.canPlayType($scope.audio.content_type).length) {
-                pauseFuncArray.push(pause);
+                audioList.push(audio);
 
-                $scope.audio.formatedDuration = audioElement.duration ? formatDuration(audioElement.duration) : '';
+                $scope.audio.formatedDuration = audioElement.duration ? audio.formatDuration(audioElement.duration) : '';
             } else {
-                $scope.audio.formatedDuration = formatDuration($scope.audio.duration / 1000);
+                $scope.audio.formatedDuration = audio.formatDuration($scope.audio.duration / 1000);
             }
 
             audioElement.addEventListener('loadedmetadata', function() {
                 $scope.$apply(function() {
-                    setInitDuration();
+                    audio.setInitDuration();
                 });
             }, false);
 
@@ -60,7 +65,7 @@ return ['$window', function($window) {
                 if (audioElement.duration && !audioElement.paused) {
                     $scope.$apply(function() {
                         var lastTime = audioElement.duration - audioElement.currentTime;
-                        $scope.audio.formatedDuration = formatDuration(lastTime);
+                        $scope.audio.formatedDuration = audio.formatDuration(lastTime);
                     });
                 }
                 
@@ -69,21 +74,21 @@ return ['$window', function($window) {
             audioElement.addEventListener('ended', function() {
                 $scope.$apply(function() {
                     $scope.audio.playing = false;
-                    setInitDuration();
+                    audio.setInitDuration();
                 });
             }, false);
 
             $scope.audio.controller = function() {
                 if (!audioElement.canPlayType($scope.audio.content_type).length) {
-                    downloadAudio();
+                    audio.downloadAudio();
                 } else {
                     if (audioElement.paused) {
-                        pauseAllAudios();
-                        audioElement.src = $scope.audio.content;
-                        audioElement.play();
-                        $scope.audio.playing = true;
+                        _.each(audioList, function(item) {
+                            item.pause();
+                        });
+                        audio.play();
                     } else {
-                        pause();
+                        audio.pause();
                     }
                 }
                 
