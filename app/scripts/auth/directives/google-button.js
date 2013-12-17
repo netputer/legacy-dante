22713f1e7cd68da675881e6551c2f9e5bc356b9a
尼奥 <1988wangxiao@gmail.com>
2013-12-17 16:07:48 +0800

    [bugfix] Survey URL and google pop window may be blocked.

--- app/scripts/auth/directives/google-button.js
+++ app/scripts/auth/directives/google-button.js
@@ -8,13 +8,16 @@ return {
 link: function(scope, element) {
     //加载按钮的逻辑
     $window.googleSignInOnloadDefer.done(function() {
-        $window.gapi.signin.render(element[0], {
-            'clientid' : '592459906195-7sjc6v1cg6kf46vdhdvn8g2pvjbdn5ae.apps.googleusercontent.com',
-            'cookiepolicy' : 'single_host_origin',
-            'authuser' : '',
-            'apppackagename' : 'com.snappea',
-            'callback' : 'googleSignInCallback',
-            'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email'                
+        // 防止可能会被浏览器拦截弹窗
+        $window.gapi.auth.init(function() {
+            $window.gapi.signin.render(element[0], {
+                'clientid' : '592459906195-7sjc6v1cg6kf46vdhdvn8g2pvjbdn5ae.apps.googleusercontent.com',
+                'cookiepolicy' : 'single_host_origin',
+                'authuser' : '',
+                'apppackagename' : 'com.snappea',
+                'callback' : 'googleSignInCallback',
+                'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email'                
+            });
         });
         // $window.gapi.signin.go(element[0]);
     });
